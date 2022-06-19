5bf4372a5ca111ced80f5674bc6759815810bf28
cqh963852 <cqh963852@hotmail.com>
2022-06-20 00:18:49 +0800

    chore: remove effector

--- packages/app/lib/effector/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as createEffect } from "./creator/createEffect";
-export { default as createEvent } from "./creator/createEvent";
-export { default as createStore } from "./creator/createStore";
-export { default as ScopeProvider } from "./react/ScopeProvider";
-export { default as useEvent } from "./react/useEvent";
-export { default as useStore } from "./react/useStore";
-export { default as useStoreMap } from "./react/useStoreMap";
