738b62093abbc986cf89925f6e19e5370059094f
cqh963852 <cqh963852@hotmail.com>
2022-06-21 22:54:57 +0800

    feat: add workspaces create api

--- packages/app/pages/api/v1/workspaces/index.ts
+++ packages/app/pages/api/v1/workspaces/index.ts
@@ -1,12 +1,44 @@
+import { PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getToken } from "next-auth/jwt";
+
+const prisma = new PrismaClient();
 
 const workspacesHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { query, body, method } = req;
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  if (token == null) {
+    res.status(401);
+    return;
+  }
+
+  const { workspaceName } = body as {
+    workspaceName: string;
+  };
+
   switch (method) {
     case "GET": {
       break;
     }
     case "POST": {
+      if (workspaceName == null || token.sub == null) {
+        res.status(400).json({ error: "Missing workspaceName or userId" });
+        return;
+      }
+      prisma.workspace.create({
+        data: {
+          user: {
+            connect: {
+              id: token.sub,
+            },
+          },
+          permissionTag: {
+            create: {
+              name: workspaceName,
+            },
+          },
+        },
+      });
       break;
     }
     case "PATCH": {
