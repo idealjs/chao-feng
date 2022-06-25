e5e7539e6f9ba9047afe7e53f21f2c3ab03969f2
cqh963852 <cqh963852@hotmail.com>
2022-06-25 10:42:22 +0800

    feat: add mockProvider

--- packages/app/pages/api/auth/[...nextauth].ts
+++ packages/app/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,52 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
+import { Provider } from "next-auth/providers";
 import CredentialsProvider from "next-auth/providers/credentials";
 import EmailProvider from "next-auth/providers/email";
 
 const prisma = new PrismaClient();
+
+const providers: Provider[] = [
+  {
+    id: "wechat",
+    name: "wechat",
+    type: "oauth",
+    wellKnown:
+      "https://open.weixin.qq.com/connect/qrconnect?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect",
+    authorization: { params: { scope: "openid email profile" } },
+    idToken: true,
+    checks: ["pkce", "state"],
+    profile(profile) {
+      return {
+        id: profile.sub,
+        name: profile.name,
+        email: profile.email,
+        image: profile.picture,
+      };
+    },
+  },
+];
+
+const mockProvider: Provider[] = [
+  EmailProvider({
+    async generateVerificationToken() {
+      return "0000";
+    },
+    server: {
+      host: process.env.EMAIL_SERVER_HOST,
+      port: process.env.EMAIL_SERVER_PORT,
+      auth: {
+        user: process.env.EMAIL_SERVER_USER,
+        pass: process.env.EMAIL_SERVER_PASSWORD,
+      },
+    },
+    from: process.env.EMAIL_FROM,
+    sendVerificationRequest: (params) => {
+      console.log(params);
+    },
+  }),
+];
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -22,42 +64,7 @@ export default NextAuth({
       };
     },
   },
-  providers: [
-    EmailProvider({
-      async generateVerificationToken() {
-        return "0000";
-      },
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-      sendVerificationRequest: (params) => {
-        console.log(params);
-      },
-    }),
-    {
-      id: "wechat",
-      name: "wechat",
-      type: "oauth",
-      wellKnown:
-        "https://open.weixin.qq.com/connect/qrconnect?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect",
-      authorization: { params: { scope: "openid email profile" } },
-      idToken: true,
-      checks: ["pkce", "state"],
-      profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-        };
-      },
-    },
-  ],
+  providers:
+    process.env.NEXT_PUBLIC_NODE_ENV === "prod" ? providers : mockProvider,
   pages: {},
 });
