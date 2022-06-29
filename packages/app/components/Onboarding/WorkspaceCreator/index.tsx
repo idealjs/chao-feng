652380071007c366315bd266e88218d00ec3cd05
cqh963852 <cqh963852@hotmail.com>
2022-06-29 22:08:43 +0800

    feat: update schema

--- packages/app/components/Onboarding/WorkspaceCreator/index.tsx
+++ packages/app/components/Onboarding/WorkspaceCreator/index.tsx
@@ -1,6 +1,23 @@
+import { useRef } from "react";
+
+import useCreateWorkspace from "../../../hooks/useCreateWorkspace";
+
 const WorkspaceCreator = () => {
+  const ref = useRef<HTMLInputElement>(null);
+  const createWorkspace = useCreateWorkspace();
   return (
-    <div>WorkspaceCreator</div>
-  )
-}
-export default WorkspaceCreator
+    <div>
+      <input type="text" ref={ref} />
+      <button
+        onClick={async () => {
+          if (ref.current?.value != null && ref.current?.value !== "") {
+            await createWorkspace(ref.current.value);
+          }
+        }}
+      >
+        next
+      </button>
+    </div>
+  );
+};
+export default WorkspaceCreator;
--- packages/app/hooks/useCreateWorkspace.ts
+++ packages/app/hooks/useCreateWorkspace.ts
@@ -2,15 +2,17 @@ import { useCallback } from "react";
 
 const useCreateWorkspace = () => {
   return useCallback(async (name: string) => {
-    await fetch("/api/v1/workspace", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name,
-      }),
-    });
+    return (
+      await fetch("/api/v1/workspaces", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+        }),
+      })
+    ).json();
   }, []);
 };
 
