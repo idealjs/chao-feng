dd9da5fe5a9649da7792e495fb8e53980cfbc4d3
cqh963852 <cqh963852@hotmail.com>
2022-07-18 20:41:48 +0800

    feat: remove page hook's socket

--- packages/app/hooks/api/useDeletePage.ts
+++ packages/app/hooks/api/useDeletePage.ts
@@ -1,31 +1,15 @@
 import { Page } from "@prisma/client";
 import { useCallback } from "react";
 
-import { useSocket } from "../../components/SocketProvider";
-
 const useDeletePage = () => {
-  const socket = useSocket();
-  return useCallback(
-    async (params: { pageId: string }) => {
-      const { pageId } = params;
-      const res = await fetch(`/api/v1/pages/${pageId}`, {
-        method: "DELETE",
-      });
-      const page = (await res.json()) as Page;
-
-      if (page.parentId) {
-        socket?.emit("updated", {
-          updatedUrl: `/api/v1/pages/${page.parentId}`,
-        });
-      } else {
-        socket?.emit("updated", {
-          updatedUrl: `/api/v1/workspaces/${page.workspaceId}`,
-        });
-      }
-      return page;
-    },
-    [socket]
-  );
+  return useCallback(async (params: { pageId: string }) => {
+    const { pageId } = params;
+    const res = await fetch(`/api/v1/pages/${pageId}`, {
+      method: "DELETE",
+    });
+    const page = (await res.json()) as Page;
+    return page;
+  }, []);
 };
 
 export default useDeletePage;
