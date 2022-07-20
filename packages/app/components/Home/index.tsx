106972ff306beb94dfaaadd8da4265d200769431
cqh963852 <cqh963852@hotmail.com>
2022-07-20 20:28:39 +0800

    feat: update home page

--- packages/app/components/Home/index.tsx
+++ packages/app/components/Home/index.tsx
@@ -1,8 +1,7 @@
 import { useRouter } from "next/router";
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 
 import useProfile from "../../hooks/useProfile";
-import SignIn from "./SignIn";
 
 const Home = () => {
   const router = useRouter();
@@ -10,23 +9,39 @@ const Home = () => {
   const { data: session, status } = useSession();
   const profile = useProfile();
 
+  console.log("test test", status, profile, session);
+
   if (status === "loading") {
     return null;
   }
 
-  if (session == null) {
-    return <SignIn />;
-  }
-
   if (profile?.lastActive != null) {
     router.push(`/${profile.lastActive}`);
+    return null;
   }
 
   if (session != null && profile?.lastActive == null) {
     router.push("/onboarding");
+    return null;
   }
 
-  return null;
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary" onClick={() => signIn()}>
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
