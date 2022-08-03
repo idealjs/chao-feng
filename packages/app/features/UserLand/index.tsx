dd67657bfc3073ae3d9960201d569a53f4d5d8ab
cqh963852 <cqh963852@hotmail.com>
2022-08-03 08:45:29 +0800

    chore: update userland

--- packages/app/features/UserLand/index.tsx
+++ packages/app/features/UserLand/index.tsx
@@ -1,6 +1,3 @@
-import { useEffect, useState } from "react";
-import { Doc } from "yjs";
-
 import usePageId from "../../hooks/usePageId";
 import YDocProvider from "../../lib/react-yjs/src/YDocProvider";
 import SocketProvider from "../SocketProvider";
@@ -9,18 +6,8 @@ import Editor from "./Editor";
 const UserLand = () => {
   const pageId = usePageId();
 
-  const [yDoc, setYDoc] = useState<Doc | null>(null);
-  useEffect(() => {
-    const doc = new Doc();
-    setYDoc(doc);
-    return () => {
-      doc.destroy();
-      setYDoc(null);
-    };
-  }, []);
-
   return (
-    <YDocProvider yDoc={yDoc}>
+    <YDocProvider>
       <SocketProvider
         uri={process.env.NEXT_PUBLIC_WEBSOCKET_URL}
         opts={{ query: { pageId } }}
