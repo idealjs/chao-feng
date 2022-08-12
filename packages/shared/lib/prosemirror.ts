9aba7e75cc35ab00700efccdca43141fd1b75bf6
cqh963852 <cqh963852@hotmail.com>
2022-08-13 00:11:49 +0800

    fix: add schema toDom function

--- packages/shared/lib/prosemirror.ts
+++ packages/shared/lib/prosemirror.ts
@@ -3,7 +3,12 @@ import { Schema } from "prosemirror-model";
 export const schema = new Schema({
   nodes: {
     doc: { content: "paragraph+" },
-    paragraph: { content: "text*" },
+    paragraph: {
+      content: "text*",
+      toDOM(node) {
+        return ["p", 0];
+      },
+    },
     text: { inline: true },
   },
 });
