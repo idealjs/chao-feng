d1109ca8aeaf08a106dbefb7da8572c720a6615e
cqh963852 <cqh963852@hotmail.com>
2022-08-18 11:47:04 +0800

    feat: use prosemirror as editor

--- packages/app/features/Block/Text/index.tsx
+++ packages/app/features/Block/Text/index.tsx
@@ -1,27 +1,55 @@
-import { Block } from "@prisma/client";
+import { schema } from "@idealjs/chao-feng-shared/lib/prosemirror";
+import { EditorState } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { ySyncPlugin } from "y-prosemirror";
+import { Doc } from "yjs";
 
+import { useYDocSelector } from "../../../lib/react-yjs";
 import { IBaseTextBlock } from "../../../lib/type";
 
 export interface ITextBlock extends IBaseTextBlock {
   type: "text";
 }
 interface IProps {
-  block: ITextBlock;
+  blockId: string;
 }
 
 const Text = (props: IProps) => {
-  const { block } = props;
+  const { blockId } = props;
+  const ref = useRef<HTMLDivElement>(null);
+  const [editor, setEditor] = useState<EditorView | null>(null);
 
-  return (
-    <div contentEditable={true} suppressContentEditableWarning={true}>
-      {block.id}
-    </div>
-  );
+  const blockDoc = useYDocSelector((yDoc) => {
+    return yDoc?.getMap<Doc>("blockDocs").get(blockId);
+  });
+
+  const yXmlFragment = useMemo(() => {
+    return blockDoc?.getXmlFragment("prosemirror");
+  }, [blockDoc]);
+
+  useEffect(() => {
+    if (yXmlFragment) {
+      const editor = new EditorView(ref.current, {
+        state: EditorState.create({
+          schema,
+          plugins: [ySyncPlugin(yXmlFragment)],
+        }),
+      });
+      setEditor(editor);
+      return () => {
+        editor.destroy();
+        setEditor(null);
+      };
+    }
+  }, [yXmlFragment]);
+
+  return <div ref={ref}></div>;
 };
 
 export default Text;
 
-export const isTextBlock = (block?: Block): block is ITextBlock => {
+export const isTextBlock = (block?: { type: string }): block is ITextBlock => {
   if (block?.type === "text") {
     return true;
   }
