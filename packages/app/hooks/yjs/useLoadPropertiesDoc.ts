bf82ced8f0b95bbaaa54fc77abfa35e834db0b55
cqh963852 <cqh963852@hotmail.com>
2022-09-13 22:22:49 +0800

    chore: rename

--- packages/app/hooks/yjs/useLoadPropertiesDoc.ts
+++ packages/app/hooks/yjs/useLoadPropertiesDoc.ts
@@ -2,14 +2,15 @@ import { useEffect } from "react";
 
 import { useSocket } from "../../features/SocketProvider";
 
-const useInitPropertiesDoc = (blockId: string) => {
+const useLoadPropertiesDoc = (blockId: string) => {
   const socket = useSocket();
   useEffect(() => {
     if (socket == null) {
       return;
     }
+    console.debug("[debug] LOAD_PROPERTIES_DOC");
     socket.emit("LOAD_PROPERTIES_DOC", { blockId });
   }, [blockId, socket]);
 };
 
-export default useInitPropertiesDoc;
+export default useLoadPropertiesDoc;
