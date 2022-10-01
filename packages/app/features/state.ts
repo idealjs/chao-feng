24d63ad58f73bdd0c3c94194d0f886cea54e9c30
cqh963852 <cqh963852@hotmail.com>
2022-10-01 21:16:02 +0800

    feat: update state

--- packages/app/features/state.ts
+++ packages/app/features/state.ts
@@ -1,13 +1,14 @@
 import { proxy } from "valtio/vanilla";
 import { bindProxyAndYMap } from "valtio-yjs";
 import { Doc } from "yjs";
+import { YXmlFragment } from "yjs/dist/src/internals";
 
 import { IBlock, IPage } from "../lib/type";
 
 const yDoc = new Doc();
 const yMapPages = yDoc.getMap<IPage | null>("pages");
 const yMapBlocks = yDoc.getMap<IBlock | null>("blocks");
-const yMapBlockPropertiesDoc = yDoc.getMap<Doc>("docMapOfBlockProperties");
+const yMapBlockYXmlFragment = yDoc.getMap<YXmlFragment>("blockYXmlFragment");
 
 const proxyPages = proxy<Record<string, IPage | null>>({});
 const proxyBlocks = proxy<Record<string, IBlock | null>>({});
@@ -15,4 +16,4 @@ const proxyBlocks = proxy<Record<string, IBlock | null>>({});
 bindProxyAndYMap(proxyPages, yMapPages);
 bindProxyAndYMap(proxyBlocks, yMapBlocks);
 
-export { proxyBlocks, proxyPages, yDoc, yMapBlockPropertiesDoc };
+export { proxyBlocks, proxyPages, yDoc, yMapBlockYXmlFragment };
--- packages/server/src/state.ts
+++ packages/server/src/state.ts
@@ -2,11 +2,12 @@ import type { Block, Page } from "prisma/prisma-client";
 import { proxy } from "valtio/vanilla";
 import { bindProxyAndYMap } from "valtio-yjs";
 import { Doc } from "yjs";
+import { YXmlFragment } from "yjs/dist/src/internals";
 
 const yDoc = new Doc();
 const yMapPages = yDoc.getMap<Page | null>("pages");
 const yMapBlocks = yDoc.getMap<Block | null>("blocks");
-const yMapBlockPropertiesDoc = yDoc.getMap<Doc>("docMapOfBlockProperties");
+const yMapBlockYXmlFragment = yDoc.getMap<YXmlFragment>("blockYXmlFragment");
 
 const proxyPages = proxy<Record<string, Page | null>>({});
 const proxyBlocks = proxy<Record<string, Block | null>>({});
@@ -14,4 +15,4 @@ const proxyBlocks = proxy<Record<string, Block | null>>({});
 bindProxyAndYMap(proxyPages, yMapPages);
 bindProxyAndYMap(proxyBlocks, yMapBlocks);
 
-export { proxyBlocks, proxyPages, yDoc, yMapBlockPropertiesDoc };
+export { proxyBlocks, proxyPages, yDoc, yMapBlockYXmlFragment };
