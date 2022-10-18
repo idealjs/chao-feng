b5f46bf338e5e24bf086cd86fd5d13fc37c8bd61
cqh963852 <cqh963852@hotmail.com>
2022-10-18 21:37:11 +0800

    chore: remove useless lib

--- packages/app/lib/react-rxjs/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as createStore } from "./src/creator/createStore";
-export { default as ScopeProvider } from "./src/react/ScopeProvider";
-export { default as useEventHandler } from "./src/react/useEventHandler";
-export { default as useRxSubject } from "./src/react/useRxSubject";
-export { default as useStoreNext } from "./src/react/useStoreNext";
-export { default as useStoreSelector } from "./src/react/useStoreSelector";
-export { default as useStoreValue } from "./src/react/useStoreValue";
--- packages/app/lib/sns/snsContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-import Sns, { sns } from "./Sns";
-
-const snsContext = createContext<Sns>(sns);
-
-export default snsContext;
